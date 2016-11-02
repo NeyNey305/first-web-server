db425c60de6b26bf06ae2ab4cc66a0b96643ca79
Jenee Washington <jeneewashington@icloud.com>
2016-11-02 01:29:41 -0400

    Updated server

--- server.rb
+++ server.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'httparty'
+require 'nokogiri'
+
+
+
+url = "http://miami.craigslist.org/search/sof"
+response = HTTParty.get(url)
+
+dom = Nokogiri::HTML(response.body)
+job_title_links = dom.css('a.hdrlnk')
+
+job_titles = job_title_links.map(&:text)
+# pp job_titles
 
 get '/' do
-   File.read(File.join('hello.txt'))
+  #  File.read(File.join('hello.txt')) // Initial "Hello World" text
+  job_titles.split
+
 end
